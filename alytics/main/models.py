7802010720b6d1eb4f5d9d746a70941c1a20a8b7
Aleksandr <lvar-8@ya.ru>
2021-06-13 11:17:07 +0300

    Набросок модели

--- alytics/main/models.py
+++ alytics/main/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class UserFunction(models.Model):
-    pass
+    user = models.ForeignKey(User, verbose_name="Пользователь")
+    func = models.CharField(max_length=200, null=False, blank=False, verbose_name="Формула", help_text="Формула вида: t*t + 2/t")
+    graph = models.ImageField(upload_to="graphs", verbose_name="График")
+    interval = models.DurationField(verbose_name="Глубина периода моделирования в днях")
+    dt = models.DurationField(verbose_name="шаг в часах")
